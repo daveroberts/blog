f4803eafd96ec35d43abcc7a7ea0a1d7ca3ddab7
Dave Roberts <dave.a.roberts@gmail.com>
2017-09-27 12:25:14 -0400

    WIP

--- page_builder/executor.rb
+++ page_builder/executor.rb
@@ -3,6 +3,31 @@ require 'toml-rb'
 require 'erb'
 
 module PageBuilder
+  class Page
+    def initialize
+      @templates = {}
+    end
+    
+    def render(name, locals={}, layout=nil)
+      locals.each do |key, value|
+        instance_variable_set("@#{key}", value)
+      end
+      if layout != nil
+        @child = template(name).result(binding)
+        return template(layout).result(binding)
+      else
+        return template(name).result(binding)
+      end
+    end
+
+    def template(name)
+      return @templates[name] if @templates.has_key? name
+      #@templates[name] = ERB.new(File.read("./templates/#{name}.html.erb"), nil, nil, name)
+      @templates[name] = ERB.new(File.read("./templates/#{name}.html.erb"))
+      return @templates[name]
+    end
+  end
+
   class Executor
     def build
       posts = []
@@ -13,8 +38,28 @@ module PageBuilder
         rescue => e
         end
       end
+      tags = {}
       posts.each do |post|
-        File.write("dist/posts/#{post[:filename]}", post[:html])
+        post[:tags].each do |tag|
+          tags[tag] = [] if !tags.has_key? tag
+          tags[tag].push post
+        end
+      end
+      posts.each do |post|
+        html = Page.new.render('post', {
+          content: post[:content],
+          title: post[:title],
+          post: post,
+          all_posts: posts }, 'layout')
+        File.write("dist/posts/#{post[:filename]}", html)
+      end
+      tags.keys.each do |tag|
+        html = Page.new.render('tag', {
+          all_posts: posts,
+          title: "`#{tag}` posts",
+          tagged_posts: tags[tag],
+          tag: tag, }, 'layout')
+        File.write("dist/tags/#{tag}.html", html)
       end
     end
 
@@ -23,17 +68,8 @@ module PageBuilder
       content = content[(content.index('----')+5)..-1]
       post = TomlRB.parse(front_matter, symbolize_keys: true)
       post[:filename] = filename[filename.rindex('/')+1..-1]
-      b = binding
-      b.local_variable_set(:content, content)
-      b.local_variable_set(:title, post[:title])
-      post[:html] = page_template.result(b)
+      post[:content] = content
       return post
-    end
-
-    def page_template
-      return @post_template if @post_template
-      @post_template = ERB.new(File.read('./templates/post.html.erb'))
-      return @post_template
     end
   end
 end
